76421c840729702e46fcba3d11cec003a68fc558
abinadabV <abinadab.vazquez@gmail.com>
2022-07-01 11:01:19 -0500

    add: pipes

--- practices/src/app/main/pipes/pipes.component.ts
+++ practices/src/app/main/pipes/pipes.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-pipes',
   templateUrl: './pipes.component.html',
-  styleUrls: ['./pipes.component.css']
+  styleUrls: ['./pipes.component.scss']
 })
 export class PipesComponent implements OnInit {
+
+  employee = {
+    name: 'Fernando',
+    country: 'México',
+    age: '30',
+    phone: '5566998877',
+    gender: 'M'
+  }
 
   users : any[] = [
     {
@@ -16,13 +25,50 @@ export class PipesComponent implements OnInit {
       name: 'Laura',
       gender: 'M'
     },
-  ]
+  ];
 
+  items0 : any[] = [];
 
-  mapping = {
+  items1 : any[] = [
+    {
+      name: 'Pizza',
+    },
+  ];
+
+  items2 : any[] = [
+    {
+      name: 'Pizza',
+    },
+    {
+      name: 'Hamburguesa',
+    },
+  ];
+
+  items3 : any[] = [
+    {
+      name: 'Pizza',
+    },
+    {
+      name: 'Hamburguesa',
+    },
+    {
+      name: 'Papas',
+    },
+  ];
+  
+  mappingUsers = {
     'H': 'Bienvenido',
     'M': 'Bienvenida'
   }
+
+  mappingItems = {
+    '=0': 'no tiene ningún producto',
+    '=1': 'tiene un producto',
+    'other': 'tiene # productos'
+  }
+
+
+  observableNumber = interval(100);
 
   constructor() { }
 
